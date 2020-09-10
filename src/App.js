930944731a5c5a48410e82d4728954253bacc078
AndreMasteguin05 <andre.masteguin@gmail.com>
2020-09-10 00:47:55 -0300

    create login page and authenticate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import { auth } from "./firabase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, [dispatch]);
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
-          <Route path="/checkout">
-            <Checkout />
-
+          <Route path="/login">
+            <Login />
           </Route>
-          <Route path="/">
-
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route exact path="/">
+            <Header />
             <Home />
           </Route>
-
         </Switch>
-
       </div>
-
     </Router>
   );
 }
